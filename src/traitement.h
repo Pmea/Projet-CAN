3272f9a4a7e106f3bf542b753cf72130cb8bd82c
Pmea <mahe.pierre@live.fr>
2015-04-27 10:01:13 +0200

    blip blip

---
+++ src/traitement.h
@@ -0,0 +1,9 @@
+#ifndef TRAITEMENT_H
+#define TRAITEMENT_H
+
+#include "can.h"
+
+// retourne le noeud dans ses voisins rapprochant le plus de la coordonner
+int router(int x, int y);
+
+#endif /* TRAITEMENT_H */
